e3a738c16a2c98131870614928894c0729ca280a
LILYGO_L <13073925292@163.com>
2026-03-23 16:04:15 +0800

    update tca8418

--- main/keyboard_examples/tca8418/main.cpp
+++ main/keyboard_examples/tca8418/main.cpp
@@ -1,31 +1,20 @@
 /*
- * @Description: xl9535
+ * @Description: tca8418
  * @Author: LILYGO_L
  * @Date: 2025-06-13 14:20:16
- * @LastEditTime: 2025-09-15 17:28:35
+ * @LastEditTime: 2026-03-23 16:03:37
  * @License: GPL 3.0
  */
-#include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "esp_log.h"
-#include "sdkconfig.h"
-#include "t_display_p4_keyboard_config.h"
-#include "t_display_p4_driver.h"
+#include "lilygo_device_driver_library.h"
 #include "cpp_bus_driver_library.h"
 
-// auto XL9555_IIC_Bus = std::make_shared<Cpp_Bus_Driver::Hardware_Iic_1>(XL9555_SDA, XL9555_SCL, I2C_NUM_0);
+auto Xl9555_Iic_Bus = std::make_shared<Cpp_Bus_Driver::Software_Iic>(XL9555_SDA, XL9555_SCL);
 
-// auto TCA8418_IIC_Bus = std::make_shared<Cpp_Bus_Driver::Hardware_Iic_1>(TCA8418_SDA, TCA8418_SCL, I2C_NUM_0);
+auto Tca8418_Iic_Bus = std::make_shared<Cpp_Bus_Driver::Software_Iic>(TCA8418_SDA, TCA8418_SCL);
 
-auto XL9555_IIC_Bus = std::make_shared<Cpp_Bus_Driver::Software_Iic>(XL9555_SDA, XL9555_SCL);
+auto Xl9555 = std::make_unique<Cpp_Bus_Driver::Xl95x5>(Xl9555_Iic_Bus, XL9555_IIC_ADDRESS);
 
-auto TCA8418_IIC_Bus = std::make_shared<Cpp_Bus_Driver::Software_Iic>(TCA8418_SDA, TCA8418_SCL);
-
-auto XL9555 = std::make_unique<Cpp_Bus_Driver::Xl95x5>(XL9555_IIC_Bus, XL9555_IIC_ADDRESS, DEFAULT_CPP_BUS_DRIVER_VALUE);
-
-auto TCA8418 = std::make_unique<Cpp_Bus_Driver::Tca8418>(TCA8418_IIC_Bus, TCA8418_IIC_ADDRESS, DEFAULT_CPP_BUS_DRIVER_VALUE);
+auto Tca8418 = std::make_unique<Cpp_Bus_Driver::Tca8418>(Tca8418_Iic_Bus, TCA8418_IIC_ADDRESS);
 
 volatile bool Interrupt_Flag = false;
 
@@ -33,39 +22,40 @@ extern "C" void app_main(void)
 {
     printf("Ciallo\n");
 
-    Init_Ldo_Channel_Power(4, 3300);
+    Lilygo_Device_Driver::Init_Ldo_Channel_Power(4, 3300);
 
-    XL9555->begin();
-    XL9555->pin_mode(XL9555_LED_1, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
-    XL9555->pin_mode(XL9555_LED_2, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
-    XL9555->pin_mode(XL9555_LED_3, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
-    XL9555->pin_write(XL9555_LED_1, Cpp_Bus_Driver::Xl95x5::Value::LOW); // 开启led
-    XL9555->pin_write(XL9555_LED_2, Cpp_Bus_Driver::Xl95x5::Value::LOW);
-    XL9555->pin_write(XL9555_LED_3, Cpp_Bus_Driver::Xl95x5::Value::LOW);
+    Xl9555->begin();
+    Xl9555->pin_mode(XL9555_LED_1, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
+    Xl9555->pin_mode(XL9555_LED_2, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
+    Xl9555->pin_mode(XL9555_LED_3, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
+    Xl9555->pin_write(XL9555_LED_1, Cpp_Bus_Driver::Xl95x5::Value::LOW); // 开启led
+    Xl9555->pin_write(XL9555_LED_2, Cpp_Bus_Driver::Xl95x5::Value::LOW);
+    Xl9555->pin_write(XL9555_LED_3, Cpp_Bus_Driver::Xl95x5::Value::LOW);
 
-    XL9555->pin_mode(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
-    XL9555->pin_write(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Value::HIGH);
+    Xl9555->pin_mode(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Mode::OUTPUT);
+    Xl9555->pin_write(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Value::HIGH);
     vTaskDelay(pdMS_TO_TICKS(10));
-    XL9555->pin_write(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Value::LOW);
+    Xl9555->pin_write(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Value::LOW);
     vTaskDelay(pdMS_TO_TICKS(10));
-    XL9555->pin_write(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Value::HIGH);
+    Xl9555->pin_write(XL9555_TCA8418_RST, Cpp_Bus_Driver::Xl95x5::Value::HIGH);
     vTaskDelay(pdMS_TO_TICKS(10));
 
-    TCA8418->create_gpio_interrupt(TCA8418_INT, Cpp_Bus_Driver::Tool::Interrupt_Mode::FALLING,
+    Tca8418->create_gpio_interrupt(TCA8418_INT, Cpp_Bus_Driver::Tool::Interrupt_Mode::FALLING,
                                    [](void *arg) -> IRAM_ATTR void
                                    {
                                        Interrupt_Flag = true;
                                    });
 
-    // TCA8418_IIC_Bus->set_bus_handle(XL9555_IIC_Bus->get_bus_handle());
+    // Tca8418_Iic_Bus->set_bus_handle(Xl9555_Iic_Bus->get_bus_handle());
 
-    TCA8418->begin();
-    TCA8418->set_keypad_scan_window(0, 0, TCA8418_KEYPAD_SCAN_WIDTH, TCA8418_KEYPAD_SCAN_HEIGHT);
-    TCA8418->set_irq_pin_mode(Cpp_Bus_Driver::Tca8418::Irq_Mask::KEY_EVENTS);
-    TCA8418->clear_irq_flag(Cpp_Bus_Driver::Tca8418::Irq_Flag::KEY_EVENTS);
+    Tca8418->begin();
+    Tca8418->set_keypad_scan_window(0, 0, TCA8418_KEYPAD_SCAN_WIDTH, TCA8418_KEYPAD_SCAN_HEIGHT);
+    Tca8418->set_irq_pin_mode(Cpp_Bus_Driver::Tca8418::Irq_Mask::KEY_EVENTS);
+    Tca8418->clear_irq_flag(Cpp_Bus_Driver::Tca8418::Irq_Flag::KEY_EVENTS);
 
-    TCA8418->create_pwm(KEYBOARD_BL, ledc_channel_t::LEDC_CHANNEL_0, 20000);
-    TCA8418->start_pwm_gradient_time(100, 1000);
+    Tca8418->create_pwm(KEYBOARD_BL, ledc_channel_t::LEDC_CHANNEL_1, 1000000,
+                        0, ledc_mode_t::LEDC_LOW_SPEED_MODE, ledc_timer_bit_t ::LEDC_TIMER_5_BIT);
+    Tca8418->start_pwm_gradient_time(30, 1000);
 
     while (1)
     {
@@ -73,7 +63,7 @@ extern "C" void app_main(void)
         {
             Cpp_Bus_Driver::Tca8418::Irq_Status is;
 
-            if (TCA8418->parse_irq_status(TCA8418->get_irq_flag(), is) == false)
+            if (Tca8418->parse_irq_status(Tca8418->get_irq_flag(), is) == false)
             {
                 printf("parse_irq_status fail\n");
             }
@@ -82,7 +72,7 @@ extern "C" void app_main(void)
                 if (is.key_events_flag == true)
                 {
                     Cpp_Bus_Driver::Tca8418::Touch_Point tp;
-                    if (TCA8418->get_multiple_touch_point(tp) == true)
+                    if (Tca8418->get_multiple_touch_point(tp) == true)
                     {
                         printf("touch finger: %d\n", tp.finger_count);
 
@@ -93,7 +83,7 @@ extern "C" void app_main(void)
                             case Cpp_Bus_Driver::Tca8418::Event_Type::KEYPAD:
                             {
                                 Cpp_Bus_Driver::Tca8418::Touch_Position tp_2;
-                                if (TCA8418->parse_touch_num(tp.info[i].num, tp_2) == true)
+                                if (Tca8418->parse_touch_num(tp.info[i].num, tp_2) == true)
                                 {
                                     printf("keypad event\n");
                                     printf("   touch num:[%d] num: %d x: %d y: %d press_flag: %d\n", i + 1, tp.info[i].num, tp_2.x, tp_2.y, tp.info[i].press_flag);
@@ -116,7 +106,7 @@ extern "C" void app_main(void)
                         }
                     }
 
-                    TCA8418->clear_irq_flag(Cpp_Bus_Driver::Tca8418::Irq_Flag::KEY_EVENTS);
+                    Tca8418->clear_irq_flag(Cpp_Bus_Driver::Tca8418::Irq_Flag::KEY_EVENTS);
                 }
             }
 
